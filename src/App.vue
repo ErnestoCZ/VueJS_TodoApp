4d12ef6bc7134499f3d4a05769c99e38ab4d1e11
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-22 00:08:06 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  
+  <div class="flex-shrink-0 flex-col space">
+  <header class="place-items-center bg-lime-600 p-4" >
+
+    <h1>
+      TodoApp by Vue
+    </h1>
+  </header>
+
+  <div>
+
+  </div>
+
+  <footer class="place-content-center bg-lime-600 p-4">
+    footer
+  </footer>
+
+
+  </div>
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
